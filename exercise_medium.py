562f91d5ac6e7219626b2cce172b1e7e52fdc955
Gabriele Valastro <33549583+valagab@users.noreply.github.com>
2017-11-13 23:58:21 +0100

    Add files via upload

---
+++ exercise_medium.py
@@ -0,0 +1,11 @@
+# 1) Crea una classe Pet con attributi 'name', 'age' e 'type' (dog, cat, horse). 
+#   La classe deve avere il metodo 'eat' che torna 0 se il 'type' è dog o cat, altrimenti torna 1
+#   Inoltre deve avere il metodo 'jump' che torna un valore compreso tra 0 e 10 a random (cerca modulo per generare numeri random)
+# 
+# Creare una repository e per ogni esercizio fare un commit in una branch separata e fare il merge su master alla fine di ogni esercizio.
+# Ogni classe deve stare su un file separato.
+
+# Voti:
+# - Bah: non ne fai neanche uno
+# - Ci siamo quasi: ne fai due
+# - Boss: li fai tutti e 3
